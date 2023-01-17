91d444b9f976966b375a21914cd16ef461db7212
Lukie Kang <lukiek@gmail.com>
2023-01-17 00:33:34 +0000

    feat: add reactivity props and logic sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+
+	import { component_subscribe } from "svelte/internal";
+
+</script>
 <style>
     h1, h2, p {
         font-family: Arial, Helvetica, sans-serif;
@@ -74,3 +79,204 @@ This can be used in our HTML like so:
 </code></pre>
 
 
+
+<h2>Making things Reactive</h2>
+<p>A big part of web frameworks is making updates when th user interacts with the page. Svelte does this via the use of reactive variables. These are variables that are updated when the user interacts with the page. This is done via the use of the $: tag. This tag is used to define a block of code that will be run whenever the variable is updated.</p>
+<p>These are super simple to use:</p>
+
+<pre><code>
+    {`
+    <script>
+        let count = 0
+        function incrementCount() {
+		count += 1;
+	}
+    </script>
+
+    <button on:click>You have clicked me {count} times</button>
+    `}
+</code></pre>
+
+<p>This will pretty much work as you expect. Compared to React this is so much less boilerplate</p>
+
+<h2>Computed Variables</h2>
+<p>Computed variables are variables that are derived from other variables. They are updated whenever the variables they are derived from are updated. This is done via the use of the $: tag. This tag is used to define a block of code that will be run whenever the variable is updated:</p>
+<pre><code>
+    {`
+    <script>
+        let count = 0
+        function incrementCount() {
+        count += 1;
+    }
+        $: doubleCount = count * 2
+    </script>
+
+    <button on:click>You have clicked me {count} times but double that is {doubleCount}</button>
+    `}
+</code></pre>
+
+<p>We are not limited to reactively running values. we can run pretty much anything including multiple statements: </p>
+
+<pre><code>
+    <script>
+        $: {
+            console.log('The count is now', count)
+            if (count > 10) {
+                console.log('That is a lot of clicks')
+            }
+        }
+    </script>
+</code></pre>
+
+<p>The reactivity only occurs when <strong>we are assigning. Mutations of arrays or objects do not trigger updates.</strong>If we add to an array it will not update unless we assign a variable, a typical pattern is to assign to itself:</p>
+
+<pre><code>
+    <script>
+        let items = []
+        function addItem() {
+            items = [...items, 'new item']
+        }
+    </script>
+</code></pre>
+
+<p>While you can update an object or array's proerties like this too, we have a small gotcha in that we require the thing to be updated to be on the left hand side of the assignment. In other words:</p> 
+
+<pre><code>
+    <script>
+        const car = { make: 'Ford', model: 'Fiesta' }
+        car.make = 'Vauxhall' // This will update the car
+        const carModel = car.model 
+        carrModel = 'Corsa' // This will not update the car reactively
+    </script>
+</code></pre>
+
+<h2>Props</h2>
+<p>Props are variables that are passed into a component. They are defined in the script tag of the component and are passed in via the use of the {`<ComponentName prop="value" />`} syntax. Props are reactive and will update when the value is changed. They are defined as follows:</p>
+<code><pre>
+    {`
+    <script>
+        import Profile from './Profile.svelte'
+    </script>
+
+    <Profile fullName={fullName}/> // This will pass the fullName prop to the Profile component
+
+
+    // In the Profile component
+    <script>
+        export let fullName = 'John Doe'
+    </script>
+
+    <h1>{fullName}</h1>
+    `}
+</pre></code>
+
+<p>You'd need to export the prop to use it in the component_subscribe, weird I know. Using equals allows us to set a default value. We can also use the spread operator to pass an object's properties into a child component:</p>
+
+<code><pre>
+    {`
+    <script>
+        import Profile from './Profile.svelte'
+        const user = {
+            fullName: 'John Doe',
+            age: 32,
+            location: 'London'
+        }
+    </script>
+
+    <Profile {...user}/>  // This will pass the fullName, age and location props to the Profile component in one go.`}
+</pre></code>
+
+
+
+<h2>Logic</h2>
+We can conditionally render things in Svelte via the use of the {`{#if ..} tag`}:
+
+<pre><code>
+    {`
+    <script>
+       let favouriteSubject = 'french'
+    </script>
+
+    {#if favouriteSubject === 'french'}
+        <p>Bon Jour!</p>
+    {:else if favouriteSubject === 'spanish'}
+        <p>Hola!</p>
+    {:else}
+        <p>Hello</p>
+    {/if}
+    `}
+</code></pre>
+
+<p>So the # is used to denote a block of code that will be run if the condition is true. The : is used to denote a block of code that is continuing. The / is used to denote the end of the block. We can also use the {`{#each ..} tag`} to loop over an array like so:</p>
+
+<pre><code>
+    {`
+    <script>
+        let subjects = ['french', 'spanish', 'english']
+        let subjectDetails = [
+		{subject: 'French', teacher: 'Alice'}, 
+		{subject: 'English', teacher: 'Bob'}
+	];
+    </script>
+
+    {#each subjects as subject}
+        <p>{subject}</p>
+    {/each}
+    
+    // We can access the index of the item in the array by using the {index} variable
+    {#each subjects as subject, index}
+        <p>{index] {subject}</p>
+    {/each}
+
+    // We can also use destructuring to access the properties of the object
+    {#each subjectDetails as {subject, teacher}}
+        <p>{subject} is taught by {teacher}</p>
+    {/each}
+    `}
+</code></pre>
+
+<p>Keyed each blocks are important as they allow Svelte to know what DOM Node to change. Without them, if oyu modify the value of an each block it will only occur at the end of the block. We can key an each block by simply adding a key attribute to the each block:</p>
+
+<pre><code>
+    {`
+    {#each subjects as subject (subject.id)} // It is important that the key is unique and its generally better to use a string or a number rather than an object which can, in rare cases, be the same as another object.
+        <p>{subject}</p>
+    {/each}
+    `}
+</code></pre>
+
+<p>Each can also be used for getting asynchronous data: </p>
+
+<pre><code>
+    {`
+    <script>
+        async function getSubjects() {
+            const response = await fetch('https://example.com/subjects')
+            const data =  response.json();
+        if (response.ok) {
+            return data;
+        } else {
+            throw new Error(data)
+        }
+
+        let promise = getSubjects()
+
+        function handleClick() {
+            promise = getSubjects()
+        }
+    </script>
+
+    {#await promise}
+      <p>Loading...</p>
+    {:then subjects}
+        {#each subjects as subject}
+            <p>{subject}</p>
+        {/each}
+    {:catch error}
+        <p>{error.message}</p>
+   {/await}
+    `}
+</code></pre>
+
+
+<h2>Events</h2>
